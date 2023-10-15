83210983a500c580f66abbbdb4c14f1d7251a94f
Gustavo Mendonça <alf4r6@gmail.com>
2023-10-15 00:38:24 -0300

    feat: navbar

--- src/components/global/navbar/NavButtons.tsx
+++ src/components/global/navbar/NavButtons.tsx
@@ -5,21 +5,26 @@ import { tv } from "tailwind-variants";
 interface NavButtonsProps {
 	children: ReactNode;
 	href: string;
-	textColor?: "grey" | "green";
+	textColor?: "grey" | "green" | "login";
 }
 
 const link = tv({
-	base: "font-normal text-xl ",
+	base: "font-normal text-xl transition-colors duration-200",
 	variants: {
 		textColor: {
 			grey: "text-gray-700 hover:text-gray-800",
-			green: "text-vs-green-500 font-semibold hover:text-green-600",
-		}
-	}
+			green: "text-vs-green-500 font-semibold hover:text-vs-green-400",
+			login: "px-5 py-2 rounded-xl font-semibold text-base bg-vs-green-500 hover:bg-vs-green-400 text-white"
+		},
+	},
 });
 
 // font-semibold text-xl text-vs-green-500
-export function NavButtons({ children, href, textColor = "grey" }: NavButtonsProps) {
+export function NavButtons({
+	children,
+	href,
+	textColor = "grey",
+}: NavButtonsProps) {
 	return (
 		<Link href={href} className={link({ textColor })}>
 			{children}
--- src/components/global/navbar/Navbar.tsx
+++ src/components/global/navbar/Navbar.tsx
@@ -1,22 +1,40 @@
 import Image from "next/image";
 import { NavButtons } from "./NavButtons";
+import Link from "next/link";
 
 export function Navbar() {
 	return (
-		<nav className="bg-white mx-40 rounded-[20px] h-20 flex items-center">
-			<div className="ml-10">
-				<Image
-					src="/logo.svg"
-					alt="bang.in"
-					priority
-					quality={90}
-					width={105}
-					height={31}
-				/>
+		<nav className="bg-white mx-40 px-10 rounded-[20px] h-20 flex items-center justify-between">
+			<div className="">
+				<Link href="/">
+					<Image
+						src="/logo.svg"
+						alt="bang.in"
+						priority
+						quality={90}
+						width={105}
+						height={31}
+					/>
+				</Link>
+			</div>
+
+			<div className="flex gap-12">
+				<NavButtons href="/" textColor="green">
+					Home
+				</NavButtons>
+				<NavButtons href="/service" textColor="grey">
+					Service
+				</NavButtons>
+				<NavButtons href="/product" textColor="grey">
+					Product
+				</NavButtons>
+				<NavButtons href="/about" textColor="grey">
+					About
+				</NavButtons>
 			</div>
 
 			<div className="">
-				<NavButtons href="/" textColor="green">Home</NavButtons>
+				<NavButtons href="/login" textColor="login">Register</NavButtons>
 			</div>
 		</nav>
 	);
